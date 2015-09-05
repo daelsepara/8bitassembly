e6aa7abb0d31c95eccbbda15afdbd9f2e8ea9a75
Dael <dael.separa@gmail.com>
2015-09-06 01:22:40 +0800

    Add more comments for readability

--- prime.asm
+++ prime.asm
@@ -2,147 +2,160 @@
 ; Lacks display. adding print routine ruins the stack
 
 start:
-	CALL prime
-end:	
-	HLT
 
-count:	DB 0
-sumL:	DB 0
-sumH:	DB 0
-lsb:	DB 0x00
-msb: 	DB 0x00
-divL:	DB 0
-divH:	DB 0
+ CALL prime
+
+end:
+
+ HLT
+
+count: DB 0
+sumL: DB 0
+sumH: DB 0
+lsb: DB 0x00
+msb:  DB 0x00
+divL: DB 0
+divH: DB 0
 
 main:
-	
-	; main loop - check if we have reached 1000
-	MOV A, [lsb]
-	CMP A,0xE8
-	JNZ nz1
-	MOV A, [msb]
-	CMP A,0x03
-	JZ end
-nz1:
-	INC A
-	MOV [lsb], A
-	JNC .nc1
-	MOV A,[msb]
-	INC A
-	MOV [msb],A
+ 
+ ; main loop - check if we have reached 1000
+ MOV A, [lsb]
+ CMP A,0xE8
+ JNZ .nz1
+ MOV A, [msb]
+ CMP A,0x03
+ JZ end
 
-.nc1:	
-	MOV A,[lsb]
-	MOV B,A
-	
-	; do not process even numbers
-	AND B, 1
-	JNZ prime
-	
-	; handle the case of 2
-	CMP A,2
-	JNZ main
-	MOV A,[msb]
-	OR A, A 
-	JNZ main
-	
-prime:	
-	; copy number into divisors
-	MOV A, [lsb]
-	MOV [divL], A	
-	MOV A, [msb]
-	MOV [divH], A
+.nz1:
+
+ INC A
+ MOV [lsb], A
+ JNC .nc1
+ MOV A,[msb]
+ INC A
+ MOV [msb],A
+
+.nc1:
+ 
+ MOV A,[lsb]
+ MOV B,A
+ 
+ ; do not process even numbers
+ AND B, 1
+ JNZ prime
+ 
+ ; handle the case of 2
+ CMP A,2
+ JNZ main
+ MOV A,[msb]
+ OR A, A 
+ JNZ main
+ 
+prime:
+
+ ; copy number into divisors
+ MOV A, [lsb]
+ MOV [divL], A 
+ MOV A, [msb]
+ MOV [divH], A
 
 divisors:
-	
-	; move on to the next divisor
-	MOV A, [divL]
-	OR A, A
-	JNZ .nz2
-	
-	MOV A, [divH]
-	OR A, A
-	JZ main
-	
-	DEC A
-	MOV [divH], A
-	
-.nz2:	
+ 
+ ; move on to the next divisor
+ MOV A, [divL]
+ OR A, A
+ JNZ .nz2
+ 
+ MOV A, [divH]
+ OR A, A
+ JZ main
+ 
+ DEC A
+ MOV [divH], A
+ 
+.nz2: 
 
-	MOV A, [divL]
-	DEC A
-	MOV [divL], A
+ MOV A, [divL]
+ DEC A
+ MOV [divL], A
 
-	; do not process even divisors
-	AND A,1
-	JZ divisors
+ ; do not process even divisors
+ AND A,1
+ JZ divisors
 
 divstart:
 
-	MOV A, [lsb]
-	MOV D, [msb]
-	MOV C, [divL]
-	MOV B, [divH]
-	
+ MOV A, [lsb]
+ MOV D, [msb]
+ MOV C, [divL]
+ MOV B, [divH]
+ 
 .divcheck:
-	
-	; check if DA > BC
-	CMP D,B		
-	JB .divend
-	JNZ .divLoop	
-	CMP A,C
-	JB  .divend
+ 
+ ; check if DA > BC
+ CMP D,B  
+ JB .divend
+ JNZ .divLoop 
+ CMP A,C
+ JB  .divend
 
 .divLoop:
-	
-	; Division main loop
-	SUB D,B
-	SUB A,C
-	JNC .divcheck
-	
-	; handle any overflow from previous SUB operation
-	INC A		
-	DEC D
-	JMP .divcheck
+ 
+ ; division main loop
+ SUB D,B
+ SUB A,C
+ JNC .divcheck
+ 
+ ; handle any overflow from previous SUB operation
+ INC A  
+ DEC D
+ JMP .divcheck
 
 .divend:
 
-	; check if number was divisible
-	OR A, A
-	JNZ divisors
+ ; check if number was divisible
+ OR A, A
+ JNZ divisors
 
-	; check if divisor == 1
-	CMP C,1
-	JNZ main
-	OR B,B
-	JNZ main
+ ; check if divisor == 1
+ CMP C,1
+ JNZ main
+ OR B,B
+ JNZ main
 
-	; check if number >= 2
-	MOV A, [lsb]
-	CMP A, 2
-	JNB .nz3
-	MOV A, [msb]
-	
-	OR A, A
-	JNZ main 
+ ; check if number >= 2
+ MOV A, [lsb]
+ CMP A, 2
+ JNB .nz3
+ 
+ MOV A, [msb]
+ OR A, A
+ JZ main 
 
 .nz3:
-	
-	; prime number found
-	MOV A,[count]
-	INC A
-	MOV [count], A
-	
-	MOV A,[lsb]
-	MOV D,[msb]
-	MOV B,[sumL]
-	MOV C,[sumH]
-	ADD A,B
-	JNC .nc2
-	INC C
+ 
+ ; prime number found
+ MOV A,[count]
+ INC A
+ MOV [count], A
+ 
+ ; compute running sum
+ MOV A,[lsb]
+ MOV D,[msb]
+ MOV B,[sumL]
+ MOV C,[sumH]
+ 
+ ADD A,B
+ JNC .nc2
+ 
+ ; handle overflow
+ INC C
+
 .nc2:
-	ADD C,D
-	MOV [sumL],A
-	MOV [sumH],C
-	
-	JMP main
+
+ ADD C,D
+ MOV [sumL],A
+ MOV [sumH],C
+ 
+ JMP main
