aed0d88e2e1d273d24257b2781250e8bbdc5cb18
Dael <dael.separa@gmail.com>
2015-09-09 14:17:42 +0800

    reformatted comments

--- 1d-automata.asm
+++ 1d-automata.asm
@@ -4,53 +4,54 @@
 cellval:
 	; Determine cell value. Use periodic boundary conditions
 	;
-	; Inputs:
+	; Inputs:	A - offset of current cell
 	;
-	; A - offset of current cell
-	;
-	; Returns:
-	;
-	; A - cell value
+	; Returns:	A - cell value
 	PUSH C
 	PUSH D
 
+	; cell's exact location in memory (world)
 	MOV D, world
 	ADD A, D
 	MOV C, A
 	MOV B, A
 	
-	; determine left neighbor
+	; ... left neighbor
 	DEC B
 	CMP B, world
 	JNBE .nz1
-	MOV B, worldend
+	MOV B, w_end
 
 .nz1:
-	; determine right neighbor
+	; ... right neighbor
 	INC C
-	CMP C, worldend
+	CMP C, w_end
 	JBE .nz2
 	MOV C, world
 
 .nz2:
-	; compute cell value
-	MOV D,[A]	
-	XOR A,A		; cell value = 0
-	OR D,D		; check if current cell is alive
+	; intialize cell value to 0
+	MOV D,[A]
+	XOR A,A	
+	
+	; if current cell is alive, cell value += 2
+	OR D,D		
 	JZ .z1
-	MOV A, 2	; cell value += 2
+	MOV A, 2
 
 .z1:
-	MOV D,[B]	; check left neighbor
+	; if left neighbor is alive, cell value += 4
+	MOV D,[B]	
 	OR D,D
 	JZ .z2
-	ADD A, 4	; cell value += 4
+	ADD A, 4
 
 .z2:
-	MOV D, [C]	; check right neighbor
+	; if right neighbor is alive, cell value += 1
+	MOV D, [C]
 	OR D,D
 	JZ .z3
-	ADD A, 1	; cell value += 1
+	ADD A, 1
 	
 .z3:
 	POP D
@@ -60,58 +61,59 @@ cellval:
 automata:
 	; C = 0
 	XOR C,C
-	; point to display offset
+	; point to display
 	MOV D, 232
 .loop:
 	; compute cell value
 	MOV A, C
 	CALL cellval
 	
-	; apply rule
+	; apply rule to current cell, set '1' if alive
 	MOV B,1
 	SHL B,A
 	AND B,[rule]
 	MOV B, 0
 	JZ .set
-	; cell is alive
 	MOV B, '1'	
 
 .set:
-	; set cell state on display
+	; display cell state
 	MOV [D], B
+
 	; process next cell
 	INC D
 	INC C
 	CMP C, 24
 	JNZ .loop
-	; initialize copy-operation 
+
+	; copy cell state (display -> world)
 	MOV A, world
 	MOV D, 232
 	MOV C, 24
 
 .copy:
-	; copy cells (display -> world)
+	; live cells ('1' on display) are copied as 1, 0 otherwise
 	MOV B,[D]
-	; this works because '1' has bit 0 = 1
 	AND B,1
 	MOV [A],B
+	
 	; copy next cell
 	INC A
 	INC D
 	DEC C
 	JNZ .copy
-	; generation = generation + 1
-	MOV A, [genL]
+	
+	; increment generation, handle overflow
+	MOV A, [genL]	
 	INC A
 	MOV [genL], A
 	MOV A, [genH]
 	JNC .nc1
-	; handle overflow
 	INC A
 	MOV [genH], A
 
 .nc1:
-	; check if we have reached iteration (generation) limit
+	; check if iteration limit was reached
 	CMP A, [limH]
 	JNZ automata
 	MOV A, [genL]
@@ -119,39 +121,36 @@ automata:
 	JNZ automata
 	RET
 
-; 1D-CA Rule
-rule:
-	DB 22
-; initial configuration of the world (1 => alive, 0 => dead)
-world:
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 1
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-	DB 0
-worldend:
-	DB 0
-; generations
-genL:	DB 0
+rule:	DB 22 	; 1D-CA Rule to apply
+world:	DB 0	; initial configuration of the world (1 => alive, 0 => dead)
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 1
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+		DB 0
+w_end:	DB 0
+
+; generations (low, high bytes)
+genL:	DB 0	
 genH:	DB 0
-; limit
-limL:	DB 0xE8
+
+; iteration limit (low, high bytes)
+limL:	DB 0xE8	
 limH:	DB 0x03
