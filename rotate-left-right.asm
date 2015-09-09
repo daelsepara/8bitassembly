d37282b136216186eb66420910d904ea1eea2096
Dael <dael.separa@gmail.com>
2015-09-10 06:52:26 +0800

    ROR/ROL: Sample implementation of binary rotate operations

---
+++ rotate-left-right.asm
@@ -0,0 +1,43 @@
+; Rotate demo (ROR, ROL)
+
+	MOV A, [num]
+	CALL .ror
+	CALL .rol
+
+	HLT
+
+; Rotate right	
+.ror:
+	
+	MOV B, A
+	; rotate by shifting right once
+	SHR A, 1
+	; check bit 0
+	AND B, 1
+	JZ .rend
+	; copy bit 0 into bit 7
+	OR A, 0x80
+
+.rend:	
+	
+	RET
+
+; Rotate left
+.rol:
+	
+	MOV B, A
+	; rotate by shifting left once
+	SHL A, 1
+	; check bit 7
+	AND B, 0x80
+	JZ .lend
+	; copy bit 7 into bit 0
+	OR A, 1
+
+.lend:
+
+	RET
+	
+num:
+	; input: 10000001b
+	DB 0x81
